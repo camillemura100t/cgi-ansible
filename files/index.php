1fc2391798fc172cecf87d52546aac85ca8897f9
cdufour <opusidea@gmail.com>
2020-09-17 12:31:28 +0000

    CAAC Ansible

---
+++ files/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website</title>
+</head>
+<body>
+    <h1>Website</h1>
+
+    <?php
+        $message = 'PHP vous parle';
+        echo $message ;
+    ?>
+</body>
+</html>
